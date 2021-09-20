4a4c6c91b8a82b7c34136ec3c7ed4fb7747efbae
Kip <Kipling.Dunlap@gmail.com>
2021-09-19 23:41:11 -0400

    First DOM manip scripts

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,13 @@
-var itself = document.getElementById(#quiz)
+var itself = document.getElementById("quiz");
+console.log("quiz");
 
-itself = innerText: "New Words"; 
+
+
+itself.innerHTML = "<button> Start Quiz </button>";
+itself.addEventListener("Click", quiz);
+
+var quiz() {
+    itself.innerText = "New Words";
+    
+    // itself.innerHTML = "<label>Question</label> <field>Answer\</field>";
+}
