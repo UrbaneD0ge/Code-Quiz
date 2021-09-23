05a86e0558c1a2d530026a041b871c9cfada2b4a
Kip <88916382+UrbaneD0ge@users.noreply.github.com>
2021-09-22 22:33:01 -0400

    timer fixed

--- assets/script.js
+++ assets/script.js
@@ -1,49 +1,50 @@
 var quiz = document.querySelector('.quiz');
 var question = document.querySelector('.question');
 var answer = document.querySelector('.answer');
-var button = document.getElementById('startQuiz')
-var finalScore = document.querySelector
-button.addEventListener('click',startQuiz);
-
-var timeRemaining = 0;
+var button = document.getElementById('startQuiz');
+var finalScore = document.querySelector('#scoreBox');
+var timer = document.querySelector('#elapse')
 var highScores = [];
 var score = '';
+// var timeRemaining = 0;
 var win = false;
 var lose = false;
 
-const qBank = {Q1:"What is the letter?",A1a:"A",A1b:"B",A1c:"C",A1d:"D",Q2:"What is the color?",A2a:"red",A2b:"orange",A2c:"yellow",A2d:"blue",Q3:"What is the fruit?",A3a:"Pineapple",A3b:"Mango",A3c:"Banana",A3d:"Raspberry"}
-// quiz.innerText = "NEW Words!";
+button.addEventListener('click',startQuiz);
+
+const qBank = {Q1:"What is the letter?",A1a:"A",A1b:"B",A1c:"C",A1d:"D",Q2:"What is the color?",A2a:"red",A2b:"orange",A2c:"yellow",A2d:"blue",Q3:"What is the fruit?",A3a:"Pineapple",A3b:"Mango",A3c:"Banana",A3d:"Raspberry"};
+
+const aBank = ["A1c", "A2d", "A3a"];
 
 function startQuiz() {
-    quiz.style.display = 'none';
     button.style.display = 'none';
-    quiz.textContent = "QQQ";
+    // timer.style.display = 'none';
+    quiz.textContent = qBank.Q1
+
+    // answer.innerHTML.append('<li>' + 'Answer' )
     clock()
-    console.log(qBank[1])
-    // question.innerText = qBank[1];
-    // for(){answer.appendChild('<button>'+ )
 }
 
 // Countdown Timer
-var timeRemaining = 120;
-
+var timeRemaining = 60;
+console.log(timer)
 function clock() {
-var timeInterval = setInterval(function() {
-    if (timeRemaining > 1) {
-        countdown.textContent = timeRemaining + 'seconds left';
-        timeRemaining--;
-    } else if (timeRemaining == 1) {
-        countdown.textContent = timeRemaining + 'second left!!';
-        timeRemaining--;
-        } else {
-            timeRemaining.textContent = '';
+    var timeInterval = setInterval(function() {
+        timer.textContent = timeRemaining;
+        if (timeRemaining > 1) {
+            // timer.textContent = timeRemaining;
+            timeRemaining--;
+        } else if (timeRemaining == 1) {
+            timeRemaining--;
+            } else {
+                timeRemaining.textContent = '';
+                clearInterval(timeInterval);
+                // lose();
+            }
+        if (win == true) {
             clearInterval(timeInterval);
-            // lose();
+            score = timeRemaining;
+            // finalScoreFunction
         }
-    if (win == true) {
-        clearInterval(timeInterval);
-        score = timeRemaining;
-        // finalScoreFunction
-    }
-, 1000);
+    }, 1000);
 }
