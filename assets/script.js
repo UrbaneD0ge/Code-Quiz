01e0eedcb58ba4824abe7c3e8fa2b7b4589284c3
Kip <88916382+UrbaneD0ge@users.noreply.github.com>
2021-10-21 19:21:40 -0400

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -11,6 +11,7 @@ var win = false;
 var lose = false;
 var response;
 
+<<<<<<< Updated upstream
 button.addEventListener('click',startQuiz);
 
 const qBank = {Q1:"What is the letter?",A1:["A","B","C","D"],Q2:"What is the color?",A2:["red","orange","yellow","blue"],Q3:"What is the fruit?",A3a:["Pineapple","Mango","Banana","Raspberry"]}
@@ -71,3 +72,19 @@ function arrangeScore() {
     var someArray = json.parse(localStorage.getitem('score'));
 }
 // .sort
+=======
+itself.innerHTML = "<button> Start Quiz </button>";
+itself.addEventListener("Click", quiz);
+
+var letterChoice = quiz() {
+    itself.innerText = "New Words";
+    
+    // itself.innerHTML = "<label>Question</label> <field>Answer\</field>";
+    answerValidate();
+}
+
+var answerValidate(letterChoice) {
+    // look back at timer mechanism from unit 4 activities
+    if letterChoice.value && timer.value > 0;
+}
+>>>>>>> Stashed changes
