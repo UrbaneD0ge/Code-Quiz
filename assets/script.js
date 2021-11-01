d259c579483884da4ea4efc5f634c71b29aec3c0
Kip <88916382+UrbaneD0ge@users.noreply.github.com>
2021-11-01 19:05:05 -0400

    microchange

--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,6 @@ var win = false;
 var lose = false;
 var response;
 
-<<<<<<< Updated upstream
 button.addEventListener('click',startQuiz);
 
 const qBank = {Q1:"What is the letter?",A1:["A","B","C","D"],Q2:"What is the color?",A2:["red","orange","yellow","blue"],Q3:"What is the fruit?",A3a:["Pineapple","Mango","Banana","Raspberry"]}
@@ -72,19 +71,3 @@ function arrangeScore() {
     var someArray = json.parse(localStorage.getitem('score'));
 }
 // .sort
-=======
-itself.innerHTML = "<button> Start Quiz </button>";
-itself.addEventListener("Click", quiz);
-
-var letterChoice = quiz() {
-    itself.innerText = "New Words";
-    
-    // itself.innerHTML = "<label>Question</label> <field>Answer\</field>";
-    answerValidate();
-}
-
-var answerValidate(letterChoice) {
-    // look back at timer mechanism from unit 4 activities
-    if letterChoice.value && timer.value > 0;
-}
->>>>>>> Stashed changes
